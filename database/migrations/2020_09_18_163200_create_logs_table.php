201382b36e1783d1441f5313a12e3b65d0e33cd1
linyis <linyis@gmail.com>
2020-09-19 00:36:30 +0800

    create table

---
+++ database/migrations/2020_09_18_163200_create_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->ipAddress('ip');
+            $table->string('UA')->default('');
+            $table->string('header')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
